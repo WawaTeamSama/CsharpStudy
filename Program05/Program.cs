02ae25004e47e200824d4bdc00c776e1446c4e4e
杨黎祚 <1242781976@qq.com>
2020-03-11 12:03:50 +0800

    Program05逻辑运算符的区别

---
+++ Program05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //定义了三个为0的整型
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            //定义两个布尔类型变量
+            bool result1, result2;
+
+            //&运算符指示在不满足前置条件的情况下,后置条件也会执行
+            result1 = num1 > 0 & num2++ > 1;
+            Console.WriteLine(result1);
+            Console.WriteLine("num2=" + num2);
+
+            //&&运算符指示在不满足前置条件的情况下,后置条件将不会执行
+            result2 = num1 > 0 && num3++ > 1;
+            Console.WriteLine(result2);
+            Console.WriteLine("num3=" + num3);
+        }
+    }
+}
