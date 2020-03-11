695573e1e75f03740097bad078f1fada9b90291b
叶陵 <1242781976@qq.com>
2020-03-11 11:09:45 +0800

    Program02

---
+++ Program02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Program02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b1 = 3;
+            byte b2 = 4;
+
+            byte b3 = (byte)(b1 + (int)b2);
+            Console.WriteLine(b3);
+        }
+    }
+}
