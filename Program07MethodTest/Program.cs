1c63fc0930e0f821fa0b05231c113b88cacfbf86
杨黎祚 <1242781976@qq.com>
2020-03-13 11:15:12 +0800

    递归算法

--- Program07MethodTest/Program.cs
+++ Program07MethodTest/Program.cs
@@ -9,10 +9,22 @@ namespace Program07MethodTest
             Console.WriteLine(Add(4,4));
             Console.WriteLine("3度的余弦值:" + Math.Cos(3));
             Console.WriteLine("2的4次方" + Math.Pow(2, 4));
+
+            Console.WriteLine("求1-6的和为" + Test(6));
         }
         public static int Add(int num1,int num2)
         {
             return num1 + num2;
         }
+        //使用递归实现求1-n的和
+        public static int Test(int n)
+        {
+            if (n == 1)
+            {
+                return n;
+            }
+            int tmp = Test(n - 1);
+            return tmp + n;
+        } 
     }
 }
