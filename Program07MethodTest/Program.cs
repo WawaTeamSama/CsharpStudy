0d36cb86c7a567b7e2c38b134d3c75540ca9f78d
杨黎祚 <1242781976@qq.com>
2020-03-13 10:51:31 +0800

    方法的基本调用方式

---
+++ Program07MethodTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program07MethodTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(4,4));
+            Console.WriteLine("3度的余弦值:" + Math.Cos(3));
+            Console.WriteLine("2的4次方" + Math.Pow(2, 4));
+        }
+        public static int Add(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
