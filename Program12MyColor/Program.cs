6f2451d0b2a31b05d4e561fbba74a38c3075e763
杨黎祚 <1242781976@qq.com>
2020-03-16 12:13:25 +0800

    16th Mar Exam

---
+++ Program12MyColor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program12MyColor
+{
+    //1.定义一个结构叫MyColor，有三个成员，分别定义为int类型的red,green,blue
+    //声明一个MyColor类型的变量，并对其进行赋值，使MyColor可以表示成一个红色
+
+        public struct MyColor
+    {
+       public int red;
+       public int green;
+       public int blue;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyColor my;
+            my.red = 1;
+            Console.WriteLine(my.red);
+
+        }
+    }
+}
