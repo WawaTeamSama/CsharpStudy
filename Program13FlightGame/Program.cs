cdc69b31284e91cc27d455dbd9876a08405474f4
杨黎祚 <1242781976@qq.com>
2020-03-16 13:37:12 +0800

    绘制地图的方法ShowMap

--- Program13FlightGame/Program.cs
+++ Program13FlightGame/Program.cs
@@ -9,15 +9,50 @@ namespace Program13FlightGame
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("图例:幸运轮盘○  地雷:☆  暂停:△   时空隧道:卍");
 
-            string[] map = new string[] {"□","□", "□", "□","☆", "○", "□", "□", "△", "□", "□", "□", "☆", "□", "□", "□", "☆", "□", "□",
+            string[] map = new string[] {"□","□","□", "□", "□","☆", "○", "□", "□", "△", "□", "□", "□", "☆", "□", "□", "□", "☆", "□", "□",
                 "卍", "□", "□", "○", "□", "卍", "□", "△", "□", "□", "□", "□", "□", "☆", "□", "□", "□", "□", "☆", "□", "○", "□","□", "□", "□","卍", "□","□", "□", "□",
             "☆", "□", "□", "□", "□","○", "□","□", "□", "□","△", "□", "□","卍","☆","□", "□", "□", "□","○", "□", "□","卍","□", "□", "□", "□","□", "□", "□","☆","□", "□",
             "○", "□","□", "□", "□","卍","□","卍","□", "□","△","☆","□", "□","□", "□","□"};
-            
-            foreach(string i in map)
-            {
-                Console.Write(i);
-            }
+
+
+            ShowMap(map);
         }
+        public static void ShowMap(string[] map)
+        {
+            for (int i = 0; i < map.Length; i++)
+            {
+              
+                if (i > 29 && i < 34)
+                {
+                    Console.WriteLine();
+                    Console.Write("                                                          " + map[i]);
+                }
+                else if (i == 34 )
+                {
+                   
+                    Console.WriteLine();
+                    Console.Write("                                                          " + map[i]);
+                    Array.Reverse(map);
+                    Console.WriteLine();
+                }
+                else if (i == 64 )
+                {
+
+                    
+                    Console.WriteLine(map[i]);
+                    Array.Reverse(map);
+
+                }
+                else if (i >= 64 && i <= 69)
+                {
+                    Console.WriteLine(map[i]);
+                }
+               
+                
+                else
+                    Console.Write(map[i]);
+
+            }
+        } 
     }
 }
