5740c7518c0fcc48a5daa5529a6d86e4fbdb75f3
杨黎祚 <1242781976@qq.com>
2020-03-16 10:15:15 +0800

    readonly

--- Program09Const/Program.cs
+++ Program09Const/Program.cs
@@ -4,17 +4,20 @@ namespace Program09Const
 {
     class Program
     {
+        static readonly int x = y *2;
+        static readonly int y = 1;
         static void Main(string[] args)
         {
-            //变量
-            var number1 = 1;
-            number1 = 2;
-            Console.WriteLine("变量:" + number1);
-            //常量 在程序中不能改变
-            const int number2 = 3;
-            //number2 = 4;
-            Console.WriteLine("常量:" + number2);
+            ////变量
+            //var number1 = 1;
+            //number1 = 2;
+            //Console.WriteLine("变量:" + number1);
+            ////常量 在程序中不能改变
+            //const int number2 = 3;
+            ////number2 = 4;
+            //Console.WriteLine("常量:" + number2);
 
+            Console.WriteLine("x:" + x + " y:" + y);
         }
     }
 }
