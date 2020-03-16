d858b8c6e3ef3b5131cbee827e7391e1dc928012
杨黎祚 <1242781976@qq.com>
2020-03-16 10:03:14 +0800

    Const

---
+++ Program09Const/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program09Const
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //变量
+            var number1 = 1;
+            number1 = 2;
+            Console.WriteLine("变量:" + number1);
+            //常量 在程序中不能改变
+            const int number2 = 3;
+            //number2 = 4;
+            Console.WriteLine("常量:" + number2);
+
+        }
+    }
+}
